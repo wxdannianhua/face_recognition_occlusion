7bdebd289e0cd930ff578fba60ddbcac297585eb
wxdannianhua <52273080+wxdannianhua@users.noreply.github.com>
2021-06-27 17:06:01 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# path imagenes folder
-path_images = "images"
-
-# umbral de recnicimiento entre 
-"""
-threshold alto --> menos restrictivo
-threshold bajo --> mas restrictivo
-"""
-threshold = 0.7
